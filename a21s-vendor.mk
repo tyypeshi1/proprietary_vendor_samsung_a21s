c5b6ff9b4808828edd9d5bf73c7dee96a6cddd9d
mustafa <mostafamostafamohamed133@gmail.com>
2025-08-03 20:01:11 +0300

    Revert "a21s: Don't declare libnfc-sec-vendor.conf as a module"
    This reverts commit e7191de0f7c552db54b8232f040cfa5c6311b4f6.

--- a21s-vendor.mk
+++ a21s-vendor.mk
@@ -6,7 +6,9 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/samsung/a21s
 
 PRODUCT_COPY_FILES += \
-    vendor/samsung/a21s/proprietary/vendor/etc/libnfc-sec-vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec-vendor.conf \
     vendor/samsung/a21s/proprietary/vendor/etc/nfc/sec_s3nrn4v_hwreg.bin:$(TARGET_COPY_OUT_VENDOR)/etc/nfc/sec_s3nrn4v_hwreg.bin \
     vendor/samsung/a21s/proprietary/vendor/etc/nfc/sec_s3nrn4v_swreg.bin:$(TARGET_COPY_OUT_VENDOR)/etc/nfc/sec_s3nrn4v_swreg.bin \
     vendor/samsung/a21s/proprietary/vendor/firmware/nfc/sec_s3nrn4v_firmware.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nfc/sec_s3nrn4v_firmware.bin
+
+PRODUCT_PACKAGES += \
+    libnfc-sec-vendor.conf
